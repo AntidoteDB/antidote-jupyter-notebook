dc3661f2c9f1112229c8c56dcfc9fa5fd3a3a207
a.thanjira <a.thanjira@gmail.com>
2018-02-09 00:23:18 +0100

    updated testRemovesMapRR in RRMapServiceTest

--- jupyter-antidote-kernel/src/test/RRMapServiceTest.java
+++ jupyter-antidote-kernel/src/test/RRMapServiceTest.java
@@ -9,18 +9,14 @@ public class RRMapServiceTest extends AbstractAntidoteTest {
     private RRMapService map_service;
     private IntegerService int_service;
     private CounterService counter_service;
-    private FatCounterService fatcounter_service;
     private SetService set_service;
-    private RegisterService register_service;
 
     public RRMapServiceTest() {
         super();
         map_service = new RRMapService();
         int_service = new IntegerService();
         counter_service = new CounterService();
-        fatcounter_service = new FatCounterService();
         set_service = new SetService();
-        register_service = new RegisterService();
     }
 
     @Test
@@ -47,24 +43,16 @@ public class RRMapServiceTest extends AbstractAntidoteTest {
     public void testRemovesMapRR() {
 
         MapKey mapKey = map_service.getKey("map_rr_test_removes_map_key");
-        CounterKey x_key = fatcounter_service.getKey("map_rr_test_removes_x_key");
-        CounterKey y_key = fatcounter_service.getKey("map_rr_test_removes_y_key");
+        SetKey x_key = set_service.getKey("map_rr_test_removes_x_key");
+        SetKey y_key = set_service.getKey("map_rr_test_removes_y_key");
 
-        antidoteService.applyUpdate(map_service.updateMap(mapKey, fatcounter_service.incrementFatCounter(x_key,1),
-                                                                fatcounter_service.incrementFatCounter(y_key,2)));
-
-        int readValue_y = (Integer) antidoteService.readKeyInMap(mapKey, x_key);
-        assertEquals(1, readValue_y);
-
-        int readValue_z = (Integer) antidoteService.readKeyInMap(mapKey, y_key);
-        assertEquals(2, readValue_z);
-
-        antidoteService.applyUpdate(map_service.updateMap(mapKey, fatcounter_service.resetFatCounter(y_key)));
+        antidoteService.applyUpdate(map_service.updateMap(mapKey, set_service.addToSet(x_key,"A")));
         antidoteService.applyUpdate(map_service.removeKey(mapKey, x_key));
-        antidoteService.applyUpdate(map_service.removeKey(mapKey, y_key));
+        antidoteService.applyUpdate(map_service.updateMap(mapKey, set_service.addToSet(y_key,"A")));
+        antidoteService.applyUpdate(map_service.updateMap(mapKey, set_service.removeFromSet(y_key,"A")));
 
         MapKey.MapReadResult readValue_map = (MapKey.MapReadResult) antidoteService.readByKey(mapKey);
-        assertEquals(1, readValue_map.size());
+        assertEquals(0, readValue_map.size());
     }
 
     @Test
